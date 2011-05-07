e886e956d2252a9da325b5f4b6d49c6158e2a5c3
osoumen <osoumen@c90ea579-c64c-46bc-af2b-cd8ce9dc19fb>
2011-05-07 16:07:12 +0000

    投票数が5の時にクローラーに投票を押された時にHOTが発動しないように修正
    git-svn-id: http://picopicose.com/svn/omojong/trunk@108 c90ea579-c64c-46bc-af2b-cd8ce9dc19fb

--- page_vote.php
+++ page_vote.php
@@ -33,16 +33,6 @@ else {
 	error("対象が指定されていません。");
 }
 
-//解答ファイル中の得票数をインクリメントする
-if ( isset( $_SESSION['post_token'] ) && $_SESSION['post_token'] === $in['token'] ) {
-	$sql = sprintf( "UPDATE %s SET votes = votes + IF (votes < 999,1,0) WHERE id = %d", $kaitou_table_name, $in['ansnum'] );
-	$query = mysql_query( $sql, $link );
-	if ( !$query ) {
-		error("範囲外の解答を指定しています。");
-	}
-}
-unset( $_SESSION['post_token'] );
-
 //投票した解答を得る
 $sql = sprintf( "SELECT content,author,votes FROM %s WHERE id = %d", $kaitou_table_name, $in['ansnum'] );
 $query = mysql_query( $sql, $link );
@@ -52,20 +42,30 @@ while ( $row = mysql_fetch_array( $query, MYSQL_NUM ) ) {
 	$votes = $row[2];
 }
 
-//HOT機能(一定数の得票数の作品を自動でツイートする)
-if ( $votes == $g_hot_votes ) {
-	$pre = 'おみごと！＜';
-	$url = '＞ ' . $g_scripturl . '?' .$pastlog_param_name. '=' . $in[$pastlog_param_name] . '#' . $in['ansnum'];
+//解答ファイル中の得票数をインクリメントする
+if ( isset( $_SESSION['post_token'] ) && $_SESSION['post_token'] === $in['token'] ) {
+	$sql = sprintf( "UPDATE %s SET votes = votes + IF (votes < 999,1,0) WHERE id = %d", $kaitou_table_name, $in['ansnum'] );
+	$query = mysql_query( $sql, $link );
+	if ( !$query ) {
+		error("範囲外の解答を指定しています。");
+	}
 	
-	//合計文字数140文字をオーバーしていたら本文を縮める
-	$max_len = 140 - mb_strlen( $pre . $url );
-	$sentence = mb_strimwidth( $sentence, 0, $max_len, '…' );
-	$msg = $pre . $sentence . $url;
-	
-	// OAuthオブジェクト生成
-	$to = new TwitterOAuth(CONSUMER_KEY,CONSUMER_SECRET,ACCESS_TOKEN,ACCESS_TOKEN_SECRET);
-	$req = $to->OAuthRequest("https://twitter.com/statuses/update.xml","POST",array("status"=>$msg));
+	//HOT機能(一定数の得票数の作品を自動でツイートする)
+	if ( $votes == $g_hot_votes ) {
+		$pre = 'おみごと！＜';
+		$url = '＞ ' . $g_scripturl . '?' .$pastlog_param_name. '=' . $in[$pastlog_param_name] . '#' . $in['ansnum'];
+		
+		//合計文字数140文字をオーバーしていたら本文を縮める
+		$max_len = 140 - mb_strlen( $pre . $url );
+		$sentence = mb_strimwidth( $sentence, 0, $max_len, '…' );
+		$msg = $pre . $sentence . $url;
+		
+		// OAuthオブジェクト生成
+		$to = new TwitterOAuth(CONSUMER_KEY,CONSUMER_SECRET,ACCESS_TOKEN,ACCESS_TOKEN_SECRET);
+		$req = $to->OAuthRequest("https://twitter.com/statuses/update.xml","POST",array("status"=>$msg));
+	}
 }
+unset( $_SESSION['post_token'] );
 
 $pagetitle = '参加';
 $smarty->assign( 'pagetitle', $pagetitle );
